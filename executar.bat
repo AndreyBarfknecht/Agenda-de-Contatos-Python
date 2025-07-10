5c9434dc76ede87cd53b93548034e6003e77c1a0
Andrey Barfkencht Rodrigues <andrey30r@gmail.com>
2025-07-10 14:10:55 -0300

    Create executar.bat

---
+++ executar.bat
@@ -0,0 +1,10 @@
+@echo off
+title Agenda de Contatos
+echo Iniciando a Agenda de Contatos...
+echo.
+
+python main.py
+
+echo.
+echo Programa finalizado. Pressione qualquer tecla para fechar.
+pause
